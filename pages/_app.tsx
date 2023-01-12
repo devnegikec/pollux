ad78bb8e64f0487aa44dc281d2ddb14ac63d0238
Devendra Negi <devendera.negi@gmail.com>
2023-01-13 00:51:14 +0530

    tailwind setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import {Roboto} from '@next/font/google';
+
+const roboto = Roboto({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+	variable: '--font-roboto',
+});
 
 export default function App({Component, pageProps}: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<div className={`${roboto.variable} font-sans`}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
